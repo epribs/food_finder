3779946b428e8b7056fd46d948c268b585d2fcd9
Zach Bird <zachthebird@gmail.com>
2017-06-24 13:18:26 -0500

    components are up and working

---
+++ src/components/FiltBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class FiltBox extends Component {
+  render() {
+    return (
+      <div className="FiltBox">
+        <div className="whocares">
+          <h3>Filters oughta go here, in a box tho</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FiltBox;
---
+++ src/components/UserInput.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class UserInput extends Component {
+  render() {
+    return (
+      <div className="UserInput">
+        <div className="nomatter">
+         <h3>UserInput form belongs in this spot right here</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserInput;
