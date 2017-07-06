7d4e9a843789aa0c25b103fd4c172ba3688a84d2
epribs <ericpribramsky@gmail.com>
2017-07-05 22:51:53 -0500

    adding yelpAPI

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,25 @@
-// var axios = require('Axios');
+var axios = require('Axios');
 // var APIKey = require('./ApiKey');
 var APIData = require('./APIData');
-
 var Yelp = require('node-yelp-fusion');
-
 var yelp = new Yelp({ id:APIData.client_id , secret:APIData.client_secret });
 
-module.exports = yelp;
+var helpers ={
+	runQuery: function(zip, radius) {
+		var formattedZip = zip.trim();
+		var formattedRadius = radius.trim();
+
+		console.log("Query Run");
+		return yelp.search({
+			params: {
+				location: formattedZip,
+				radius: formattedRadius
+			}
+		})
+		.then(function(res){
+			res.json(res);
+		});
+	}
+}
+
+module.exports = helpers;
